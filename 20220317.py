7228a5e177c539b5032f21b3b0d432dae1ba63d5
xodlsqkr <teinpark@naver.com>
2022-03-17 10:31:22 +0900

    first commit

---
+++ 20220317.py
@@ -0,0 +1,39 @@
+#1
+print("Hello World")
+#2
+print("Mary's cosmetics")
+#3
+print('신씨가 소리질렀다. "도둑이야".')
+#4
+print('"C:\Windows"')
+#5
+print("안녕하세요.\n만나서\t\t반갑습니다.")
+#51
+movie_rank=["닥터스트레인지","스플릿","럭키"]
+print(movie_rank)
+#52
+movie_rank.append("배트맨")
+print(movie_rank)
+#53
+movie_rank.insert(1,"슈퍼맨")
+print(movie_rank)
+#58
+nums=[1,2,3,4,5]
+print(nums[0]+nums[1]+nums[2]+nums[3]+nums[4])
+#59
+cook=["피자","김밥","만두","양념치킨","족발","피자","김치만두","쫄면","소시지","라면","팥빙수","김치전"]
+print(len(cook))
+#60
+nums=[1,2,3,4,5]
+a = nums[0]+nums[1]+nums[2]+nums[3]+nums[4]
+average = a/len(nums)
+print(average)
+#102
+print(3==5) #결과가 거짓이므로 false
+#103
+print(3<5) #결과가 참이므로 True
+#104
+x=4
+print(1<x<5) #결과가 참이므로 True
+#105
+print((3==3)and(4!=3)) #3==3과 4!=3이 모두 참이며 and이므로 True
